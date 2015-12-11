cdf5193f1d113c5d716239d6f9b2323cda84e7ea
trongtri0705 <ng.trongtri.00@gmail.com>
2015-12-11 16:54:29 +0700

    hehe

--- staff/public/assets/js/myscript.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$('.nav a').each(function () {
-		if (location.href.indexOf($(this).attr('href')) != -1) {
-			$(this).parent().addClass('active');
-			$(this).parent().parent().parent().addClass('active');
-		}
-	});
-});
